9b95654a3c6dcb6b94292f63456a35c60bce88d5
Helen F Yan <hfyan94@gmail.com>
2018-11-01 11:10:23 -0700

    Added fishing gear covariate

--- PCAAnalysis.R
+++ PCAAnalysis.R
@@ -8,16 +8,16 @@ library(tidyverse)
 library(devtools)
 library(ggbiplot)
 
-AllData1 <- read.csv('CompleteSpeciesCovariates_181012.csv')
+AllDataRaw <- read.csv('CompleteSpeciesCovariates_181101.csv')
 
 AllData <-
-  AllData1 %>%
+  AllDataRaw %>%
   # Remove saltmarsh data because there are too many NAs
   select(ISO3, NbiScale, CoastPopScale, CoastLengthScale,
          EpiScale, EstDisScale, HkExpScale, ProteinSupScale, GdpScale, HdiScale, 
          OhiScale, IuuScale, ManImpScale, ReefFisherScale, ChondLandScale, WgiScale,
          SstMeanScale, SstMaxScale, SstMinScale, PprodMeanScale, 
-         PprodMaxScale, PprodMinScale) %>%
+         PprodMaxScale, PprodMinScale, totalGearTonScale, totalGearValScale) %>%
   distinct(., ISO3, .keep_all = TRUE)
 
 # Can't just drop NAs - need to subset first to keep majority of data
@@ -28,17 +28,29 @@ AllData <-
 econ <-
   AllData %>%
   select(ISO3, GdpScale, NbiScale, OhiScale, EpiScale, HkExpScale, ReefFisherScale,
-         CoastPopScale, ChondLandScale, ProteinSupScale) %>%
+         CoastPopScale, ChondLandScale, ProteinSupScale, 
+         totalGearTonScale, totalGearValScale) %>%
+  dplyr::rename(GDP = GdpScale,
+                NBI = NbiScale,
+                OHI = OhiScale,
+                EPI = EpiScale,
+                `Fin Exp to HK` = HkExpScale,
+                `Reef Fishers` = ReefFisherScale,
+                `Coastal Population` = CoastPopScale,
+                `Total Chondrichthyes Landings` = ChondLandScale,
+                `Marine Protein Consumption` = ProteinSupScale,
+                `Fishing Gear Landed Tonnes` = totalGearTonScale,
+                `Fishing Gear Landed Value` = totalGearValScale) %>%
   drop_na()
 
-EconPCA <- prcomp(econ[, c(2:10)], center = FALSE, scale. = FALSE)
+EconPCA <- prcomp(econ[, c(2:12)], center = FALSE, scale. = FALSE)
 
 summary(EconPCA)
 EconPCA
 
 EconPlot <-
   ggbiplot(EconPCA) +
-  scale_x_continuous(limits = c(-4, 2.5)) +
+  scale_x_continuous(limits = c(-4, 3)) +
   ggtitle('Economic Covariates') +
   theme_classic()
 
